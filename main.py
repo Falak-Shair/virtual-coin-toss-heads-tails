7da0e362c1354f28c9b10c04971ab29cfb1dbc16
Falak Shair <falakshair.563@gmail.com>
2021-06-28 02:45:29 +0500

    Virtual coin toss program

---
+++ main.py
@@ -0,0 +1,19 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+print("Welcome to the virtual coin toss program")
+toss = random.randint(0,1)
+
+if toss == 1:
+  print("Heads")
+else :
+  print("Tails")
+
+
+
+
+
+
+
+
